e3c0e5ee45f13eb35428633b6b6f8af2f438f003
Ovidiu Ra2 <112311269+OvidiuRa2@users.noreply.github.com>
2024-04-02 21:34:47 +0300

    seminar 6

--- Seminar5/Seminar5Prototype/src/Main.java
+++ Seminar5/Seminar5Prototype/src/Main.java
@@ -1,5 +1,17 @@
+import model.Reteta;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<String,Double> cantiati = new HashMap<>();
+        cantiati.put("clor",2d);
+        cantiati.put("azot",5d);
+        Reteta reteta = new Reteta("paracetamol",cantiati,7d);
+
+        System.out.println(reteta);
+
+        System.out.println(reteta.cloneaza());
     }
 }
